5850df808ec8522b8bf8519446774c74ef159220
Simone <simone.monica@trailofbits.com>
2023-06-20 22:19:44 +0200

    Add filter_builtins_from_returns

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,4 @@
-use cairo_lang_sierra::extensions::lib_func::ParamSignature;
+use cairo_lang_sierra::extensions::lib_func::{OutputVarInfo, ParamSignature};
 use cairo_lang_sierra::ids::VarId;
 
 pub const BUILTINS: [&str; 7] = [
@@ -19,6 +19,21 @@ pub fn filter_builtins_from_arguments(
     signature
         .iter()
         .zip(arguments)
+        .filter(|(sig_elem, _)| {
+            !BUILTINS.contains(&sig_elem.ty.debug_name.clone().unwrap().as_str())
+        })
+        .map(|(_, arg_elem)| arg_elem)
+        .collect()
+}
+
+/// Filter the builtins from the return variables and returns only the user defined variables
+pub fn filter_builtins_from_returns(
+    signature: &[OutputVarInfo],
+    returns: Vec<VarId>,
+) -> Vec<VarId> {
+    signature
+        .iter()
+        .zip(returns)
         .filter(|(sig_elem, _)| {
             !BUILTINS.contains(&sig_elem.ty.debug_name.clone().unwrap().as_str())
         })
