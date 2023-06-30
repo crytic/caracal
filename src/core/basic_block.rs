d553ff55c9d62114075a50346bbcec30388eaec3
Simone <simone.monica@trailofbits.com>
2023-06-30 17:21:22 +0200

    Save function call in a basic block

--- src/core/basic_block.rs
+++ src/core/basic_block.rs
@@ -1,11 +1,24 @@
 use super::instruction::Instruction;
+use crate::core::function::{Function, Type};
+use std::hash::{Hash, Hasher};
 
-#[derive(Debug, Clone)]
+use cairo_lang_sierra::extensions::core::{CoreConcreteLibfunc, CoreLibfunc, CoreType};
+use cairo_lang_sierra::program::GenStatement;
+use cairo_lang_sierra::program_registry::ProgramRegistry;
+
+#[derive(Debug, Clone, Default)]
 pub struct BasicBlock {
+    function: String,
     id: usize,
     instructions: Vec<Instruction>,
     incoming_basic_blocks: Vec<usize>,
     outgoing_basic_blocks: Vec<usize>,
+    private_call: Option<Instruction>,
+    external_call: Option<Instruction>,
+    library_call: Option<Instruction>,
+    storage_variable_read: Option<Instruction>,
+    storage_variable_written: Option<Instruction>,
+    event_emitted: Option<Instruction>,
 }
 
 impl PartialEq for BasicBlock {
@@ -14,22 +27,49 @@ impl PartialEq for BasicBlock {
     }
 }
 
+impl Eq for BasicBlock {}
+
+impl Hash for BasicBlock {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.function.hash(state);
+        self.id.hash(state);
+    }
+}
+
 impl BasicBlock {
-    pub fn new(id: usize, instructions: Vec<Instruction>) -> Self {
-        BasicBlock {
+    pub fn new(
+        function: String,
+        id: usize,
+        instructions: Vec<Instruction>,
+        functions: &[Function],
+        registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    ) -> Self {
+        let mut bb = BasicBlock {
+            function,
             id,
             instructions,
-            incoming_basic_blocks: Vec::new(),
-            outgoing_basic_blocks: Vec::new(),
-        }
+            ..Default::default()
+        };
+
+        bb.analyze(functions, registry);
+
+        bb
     }
 
     pub fn len(&self) -> usize {
         self.instructions.len()
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.instructions.is_empty()
+    }
+
     pub fn get_id(&self) -> usize {
         self.id
+    }
+
+    pub fn get_function(&self) -> &str {
+        &self.function
     }
 
     pub(super) fn get_mut_id(&mut self) -> &mut usize {
@@ -50,6 +90,33 @@ impl BasicBlock {
 
     pub fn get_outgoing_basic_blocks(&self) -> &Vec<usize> {
         &self.outgoing_basic_blocks
+    }
+
+    /// Return the function call in this basic block
+    pub fn get_function_call(&self) -> Option<&Instruction> {
+        // Assume there can be only one function call in each basic block
+        // when a function call is executed sierra always check for panics so it should be true
+
+        if let Some(instruction) = &self.event_emitted {
+            return Some(instruction);
+        }
+        if let Some(instruction) = &self.external_call {
+            return Some(instruction);
+        }
+        if let Some(instruction) = &self.library_call {
+            return Some(instruction);
+        }
+        if let Some(instruction) = &self.private_call {
+            return Some(instruction);
+        }
+        if let Some(instruction) = &self.storage_variable_read {
+            return Some(instruction);
+        }
+        if let Some(instruction) = &self.storage_variable_written {
+            return Some(instruction);
+        }
+
+        None
     }
 
     pub fn add_incoming_bb(&mut self, basic_block: usize) {
@@ -84,5 +151,73 @@ impl BasicBlock {
 
     pub fn first_instruction(&self) -> Option<&Instruction> {
         self.instructions.first()
+    }
+
+    pub fn get_private_call(&self) -> &Option<Instruction> {
+        &self.private_call
+    }
+
+    pub fn get_external_call(&self) -> &Option<Instruction> {
+        &self.external_call
+    }
+
+    pub fn get_library_call(&self) -> &Option<Instruction> {
+        &self.library_call
+    }
+
+    pub fn get_storage_variable_read(&self) -> &Option<Instruction> {
+        &self.storage_variable_read
+    }
+
+    pub fn get_storage_variable_written(&self) -> &Option<Instruction> {
+        &self.storage_variable_written
+    }
+
+    pub fn get_event_emitted(&self) -> &Option<Instruction> {
+        &self.event_emitted
+    }
+
+    pub fn analyze(
+        &mut self,
+        functions: &[Function],
+        registry: &ProgramRegistry<CoreType, CoreLibfunc>,
+    ) {
+        for instruction in self.instructions.iter() {
+            if let GenStatement::Invocation(invoc) = instruction.get_statement() {
+                let lib_func = registry
+                    .get_libfunc(&invoc.libfunc_id)
+                    .expect("Library function not found in the registry");
+
+                if let CoreConcreteLibfunc::FunctionCall(f_called) = lib_func {
+                    // We search for the function called in our list of functions to know its type
+                    for function in functions {
+                        let function_name = function.name();
+                        if function_name.as_str()
+                            == f_called.function.id.debug_name.as_ref().unwrap()
+                        {
+                            match function.ty() {
+                                Type::Storage => {
+                                    if function_name.ends_with("::read") {
+                                        self.storage_variable_read = Some(instruction.clone());
+                                    } else if function_name.ends_with("::write") {
+                                        self.storage_variable_written = Some(instruction.clone());
+                                    }
+                                }
+                                Type::Event => self.event_emitted = Some(instruction.clone()),
+                                Type::Private => self.private_call = Some(instruction.clone()),
+                                Type::AbiCallContract => {
+                                    self.external_call = Some(instruction.clone())
+                                }
+                                Type::AbiLibraryCall => {
+                                    self.library_call = Some(instruction.clone())
+                                }
+                                _ => (),
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
