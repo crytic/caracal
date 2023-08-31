54c29ec4af4c5d9e076eda7dfe4e05e6d50502d8
technovision99 <25871300+technovision99@users.noreply.github.com>
2023-08-31 07:55:46 -0700

    remove duplication and cleaned up stuff

--- src/detectors/felt252_overflow.rs
+++ src/detectors/felt252_overflow.rs
@@ -1,14 +1,13 @@
 use super::detector::{Confidence, Detector, Impact, Result};
 use crate::core::compilation_unit::CompilationUnit;
 use crate::core::core_unit::CoreUnit;
-use crate::utils::filter_builtins_from_arguments;
 use cairo_lang_sierra::extensions::felt252::Felt252BinaryOperationConcrete;
 use cairo_lang_sierra::extensions::felt252::Felt252BinaryOperator;
-use cairo_lang_sierra::extensions::lib_func::ParamSignature;
-use cairo_lang_sierra::extensions::ConcreteLibfunc;
 use cairo_lang_sierra::extensions::{core::CoreConcreteLibfunc, felt252::Felt252Concrete};
 use cairo_lang_sierra::ids::VarId;
+use cairo_lang_sierra::program::GenInvocation;
 use cairo_lang_sierra::program::Statement as SierraStatement;
+use cairo_lang_sierra::program::StatementIdx;
 use std::collections::HashSet;
 
 #[derive(Default)]
@@ -41,7 +40,7 @@ impl Detector for Felt252Overflow {
             for f in functions {
                 let name = f.name();
                 // Vector for looking up future instructions
-                let statements: Vec<SierraStatement> = f.get_statements().to_owned();
+                let statements: &Vec<SierraStatement> = f.get_statements();
                 for (index, stmt) in statements.iter().enumerate() {
                     if let SierraStatement::Invocation(invoc) = stmt {
                         // Get the concrete libfunc called
@@ -53,126 +52,34 @@ impl Detector for Felt252Overflow {
                         if let CoreConcreteLibfunc::Felt252(Felt252Concrete::BinaryOperation(op)) =
                             libfunc
                         {
-                            if let Felt252BinaryOperationConcrete::WithVar(var) = op {
-                                match var.operator {
-                                    // We need to see if this is a geniune sub or an if/assert
-                                    Felt252BinaryOperator::Sub => {
-                                        // Get the return value of the sub statement
-                                        let ret_value = &invoc.branches[0].results[0];
+                            match op {
+                                Felt252BinaryOperationConcrete::WithConst(var) => {
+                                    let operation = var.operator;
 
-                                        // Look two instructions in advance, since pattern will always be sub -> store_temp -> is_zero
-
-                                        if let Some(SierraStatement::Invocation(sub_statement)) =
-                                            statements.get(index + 2)
-                                        {
-                                            // Check if felt252_is_zero uses return param of sub instruction
-
-                                            let libfunc_sub = compilation_unit
-                                                .registry()
-                                                .get_libfunc(&sub_statement.libfunc_id)
-                                                .expect(
-                                                    "Library function not found in the registry",
-                                                );
-                                            if let CoreConcreteLibfunc::Felt252(
-                                                Felt252Concrete::IsZero(_z),
-                                            ) = libfunc_sub
-                                            {
-                                                let user_params = &sub_statement.args;
-                                                if !user_params.contains(ret_value) {
-                                                    // This is a geniuine sub instruction since it isn't used by felt252_is_zero
-                                                    // Maybe we can just continue here since is_zero is only for checking branches?
-                                                    self.check_felt252_tainted(
-                                                        &mut results,
-                                                        compilation_unit,
-                                                        op.param_signatures(),
-                                                        stmt,
-                                                        invoc.args.clone(),
-                                                        &name,
-                                                    );
-                                                }
-                                            } else {
-                                                self.check_felt252_tainted(
-                                                    &mut results,
-                                                    compilation_unit,
-                                                    op.param_signatures(),
-                                                    stmt,
-                                                    invoc.args.clone(),
-                                                    &name,
-                                                )
-                                            }
-                                        }
-                                    }
-
-                                    _ => {
-                                        self.check_felt252_tainted(
-                                            &mut results,
-                                            compilation_unit,
-                                            op.param_signatures(),
-                                            stmt,
-                                            invoc.args.clone(),
-                                            &name,
-                                        );
-                                    }
+                                    self.handle_binops(
+                                        &mut results,
+                                        compilation_unit,
+                                        invoc,
+                                        statements,
+                                        index,
+                                        stmt,
+                                        &operation,
+                                        &name,
+                                    )
                                 }
-                            }
-                            if let Felt252BinaryOperationConcrete::WithConst(var) = op {
-                                match var.operator {
-                                    // Do the same as above but for constant case
-                                    Felt252BinaryOperator::Sub => {
-                                        // Get the return value of the sub statement
-                                        let ret_value = &invoc.branches[0].results[0];
+                                Felt252BinaryOperationConcrete::WithVar(var) => {
+                                    let operation = var.operator;
 
-                                        // Look two instructions in advance
-
-                                        if let Some(SierraStatement::Invocation(sub_statement)) =
-                                            statements.get(index + 2)
-                                        {
-                                            // Check if felt252_is_zero uses return param of sub instruction
-
-                                            let libfunc_sub = compilation_unit
-                                                .registry()
-                                                .get_libfunc(&sub_statement.libfunc_id)
-                                                .expect(
-                                                    "Library function not found in the registry",
-                                                );
-                                            if let CoreConcreteLibfunc::Felt252(
-                                                Felt252Concrete::IsZero(_z),
-                                            ) = libfunc_sub
-                                            {
-                                                let user_params = &sub_statement.args;
-                                                if !user_params.contains(ret_value) {
-                                                    self.check_felt252_tainted(
-                                                        &mut results,
-                                                        compilation_unit,
-                                                        op.param_signatures(),
-                                                        stmt,
-                                                        invoc.args.clone(),
-                                                        &name,
-                                                    );
-                                                }
-                                            } else {
-                                                self.check_felt252_tainted(
-                                                    &mut results,
-                                                    compilation_unit,
-                                                    op.param_signatures(),
-                                                    stmt,
-                                                    invoc.args.clone(),
-                                                    &name,
-                                                )
-                                            }
-                                        }
-                                    }
-
-                                    _ => {
-                                        self.check_felt252_tainted(
-                                            &mut results,
-                                            compilation_unit,
-                                            op.param_signatures(),
-                                            stmt,
-                                            invoc.args.clone(),
-                                            &name,
-                                        );
-                                    }
+                                    self.handle_binops(
+                                        &mut results,
+                                        compilation_unit,
+                                        invoc,
+                                        statements,
+                                        index,
+                                        stmt,
+                                        &operation,
+                                        &name,
+                                    )
                                 }
                             }
                         }
@@ -188,16 +95,13 @@ impl Felt252Overflow {
         &self,
         results: &mut Vec<Result>,
         compilation_unit: &CompilationUnit,
-        params: &[ParamSignature],
         libfunc: &SierraStatement,
-        args: Vec<VarId>,
+        args: &[VarId],
         name: &String,
     ) {
-        let user_params = filter_builtins_from_arguments(params, args);
-        println!("{:?}", user_params);
         let mut tainted_by: HashSet<&VarId> = HashSet::new();
         let mut taints = String::new();
-        for param in user_params.iter() {
+        for param in args.iter() {
             // TODO: improve when we have source mapping,can add parameter's name instead of ID
             if compilation_unit.is_tainted(name.to_string(), param.clone())
                 && !tainted_by.contains(&param)
@@ -223,7 +127,7 @@ impl Felt252Overflow {
             });
         } else {
             let msg = format!(
-                    "The function {} uses the felt252 operation {} with the user-controlled parameters: {}",
+                    "The function {} uses the felt252 operation {} with the user-controlled parameters: {}, which is not overflow safe",
                     &name,
                     libfunc,
                     taints
@@ -234,6 +138,59 @@ impl Felt252Overflow {
                 confidence: self.confidence(),
                 message: msg,
             });
+        }
+    }
+    #[allow(clippy::too_many_arguments)]
+    fn handle_binops(
+        &self,
+        results: &mut Vec<Result>,
+        compilation_unit: &CompilationUnit,
+        invoc: &GenInvocation<StatementIdx>,
+        statements: &[SierraStatement],
+        idx: usize,
+        libfunc: &SierraStatement,
+        operation: &Felt252BinaryOperator,
+        name: &String,
+    ) {
+        // Get the return value of the sub statement
+        match operation {
+            Felt252BinaryOperator::Sub => {
+                let ret_value = &invoc.branches[0].results[0];
+
+                // Look two instructions in advance
+
+                if let Some(SierraStatement::Invocation(sub_statement)) = statements.get(idx + 2) {
+                    // Check if felt252_is_zero uses return param of sub instruction
+
+                    let libfunc_sub = compilation_unit
+                        .registry()
+                        .get_libfunc(&sub_statement.libfunc_id)
+                        .expect("Library function not found in the registry");
+                    if let CoreConcreteLibfunc::Felt252(Felt252Concrete::IsZero(_)) = libfunc_sub {
+                        let user_params = &sub_statement.args;
+                        if !user_params.contains(ret_value) {
+                            self.check_felt252_tainted(
+                                results,
+                                compilation_unit,
+                                libfunc,
+                                &invoc.args,
+                                name,
+                            );
+                        }
+                    } else {
+                        self.check_felt252_tainted(
+                            results,
+                            compilation_unit,
+                            libfunc,
+                            &invoc.args,
+                            name,
+                        )
+                    }
+                }
+            }
+            _ => {
+                self.check_felt252_tainted(results, compilation_unit, libfunc, &invoc.args, name);
+            }
         }
     }
 }
