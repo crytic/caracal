d4e865ef9a006044e2ed9c5ec50a369d67a793e8
Simone <79767264+smonicas@users.noreply.github.com>
2023-09-20 15:12:52 +0200

    Support cairo project (#47)
    * Support cairo project
    
    * Update README.md

--- src/compilation/mod.rs
+++ src/compilation/mod.rs
@@ -6,6 +6,7 @@ use cairo_lang_starknet::abi::Contract;
 
 use crate::core::core_unit::CoreOpts;
 
+mod cairo_project;
 mod scarb;
 mod standard;
 mod utils;
@@ -20,8 +21,11 @@ pub fn compile(opts: CoreOpts) -> Result<Vec<ProgramCompiled>> {
         if let Ok(entries) = fs::read_dir(opts.target.as_path()) {
             for entry in entries.flatten() {
                 if entry.file_name() == "Scarb.toml" {
-                    println!("Compiling with Scarb.");
+                    println!("Compiling with Scarb. Found Scarb.toml.");
                     return scarb::compile(opts);
+                } else if entry.file_name() == "cairo_project.toml" {
+                    println!("Compiling with Cairo. Found cairo_project.toml.");
+                    return cairo_project::compile(opts);
                 }
             }
         }
